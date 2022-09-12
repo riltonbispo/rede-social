89d589be73f9691288d9d9122e5eeff1d1feb60b
riltonbispo <riltonbispo1@gmail.com>
2022-09-12 11:30:12 -0300

    admin sign in and sign out

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+
+  devise_for :admins, skip: [:registrations], controllers: {sessions: "admins/sessions"}
+
+  namespace :admin do
+    root to: "home#index"
+    resources :admins
+  end
+
+end
