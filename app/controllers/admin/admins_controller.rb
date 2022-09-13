98f16800d9aad39207eb3012cb4567feb1bd0ee3
riltonbispo <riltonbispo1@gmail.com>
2022-09-13 14:43:10 -0300

    errors

---
+++ app/controllers/admin/admins_controller.rb
@@ -0,0 +1,27 @@
+class Admin::AdminsController < AdminController 
+  def index
+    @admins = Admin.order(id: :desc)
+  end
+
+  def new
+    @admin = Admin.new
+  end
+
+  def create
+    @admin = Admin.new(form_params)
+    if @admin.save
+      redirect_to admin_admins_path
+    else
+      
+      render :new
+    end
+  end
+
+  private
+
+  def form_params
+    params.require(:admin).permit(:name, :email, :password, :password_confirmation)
+  end
+
+
+end
