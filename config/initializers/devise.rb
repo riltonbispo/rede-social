4d11db9243b8278731395010db6df0e0d6585017
riltonbispo <riltonbispo1@gmail.com>
2022-09-16 11:34:39 -0300

    atualizacao1.0

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -9,6 +9,8 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+  config.navigational_formats = ['*/*', :html, :turbo_stream]
+
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
@@ -217,7 +219,6 @@ Devise.setup do |config|
   # config.last_attempt_warning = true
 
   # ==> Configuration for :recoverable
-  #
   # Defines which key will be used when recovering the password for an account
   # config.reset_password_keys = [:email]
 
